0a4842fd58bcb2af5ebccbb715044ee3c7417341
LahiruHarshana <lharshana2002@gmail.com>
2024-03-01 10:43:18 +0530

    updated

--- hello-spring-webmvc/src/main/java/lk/ijse/gdse66/spring/bean/SpringBean.java
+++ hello-spring-webmvc/src/main/java/lk/ijse/gdse66/spring/bean/SpringBean.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SpringBean {
+    public SpringBean() {
+        System.out.println("Spring Bean Instantiated...");
+    }
 }
