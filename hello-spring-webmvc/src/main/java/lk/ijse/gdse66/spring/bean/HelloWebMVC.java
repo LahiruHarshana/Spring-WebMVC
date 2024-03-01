e405e8bd2af750453f4e6cb6db341a3a80a9d2c9
LahiruHarshana <lharshana2002@gmail.com>
2024-03-01 12:06:17 +0530

    updated

--- hello-spring-webmvc/src/main/java/lk/ijse/gdse66/spring/bean/HelloWebMVC.java
+++ hello-spring-webmvc/src/main/java/lk/ijse/gdse66/spring/bean/HelloWebMVC.java
@@ -1,8 +1,10 @@
 package lk.ijse.gdse66.spring.bean;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloWebMVC {
     public HelloWebMVC() {
         System.out.println("HelloWebMVC Instantiated...");
