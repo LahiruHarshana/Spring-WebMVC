1d0541e97c06b7ff1eb578454ae097bdff704f8e
LahiruHarshana <lharshana2002@gmail.com>
2024-04-18 09:58:02 +0530

    spring security updating

--- SpringBoot-LeaningRoom/spring-boot-with-basic-auth 2/src/main/java/lk/ijse/gdse66/springbootwithbasicauth/SpringBootWithBasicAuthApplication.java
+++ SpringBoot-LeaningRoom/spring-boot-with-basic-auth 2/src/main/java/lk/ijse/gdse66/springbootwithbasicauth/SpringBootWithBasicAuthApplication.java
@@ -1,7 +1,9 @@
 package lk.ijse.gdse66.springbootwithbasicauth;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootWithBasicAuthApplication {
@@ -10,4 +12,8 @@ public class SpringBootWithBasicAuthApplication {
 		SpringApplication.run(SpringBootWithBasicAuthApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
