7df6d9fd8c4c38afb6e49a389deddae8babc4092
LahiruHarshana <lharshana2002@gmail.com>
2024-04-18 08:15:44 +0530

    added hello spring boot

--- SpringBoot-LeaningRoom/hello-spring-boot/src/main/java/com/example/hellospringboot/HelloSpringBootApplication.java
+++ SpringBoot-LeaningRoom/hello-spring-boot/src/main/java/com/example/hellospringboot/HelloSpringBootApplication.java
@@ -1,7 +1,9 @@
 package com.example.hellospringboot;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HelloSpringBootApplication {
@@ -10,4 +12,8 @@ public class HelloSpringBootApplication {
 		SpringApplication.run(HelloSpringBootApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
