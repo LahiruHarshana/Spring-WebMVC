2f78d85c892c37a564f9585471903c98eec8b25d
LahiruHarshana <lharshana2002@gmail.com>
2024-04-18 09:55:38 +0530

    spring security basic project added

---
+++ SpringBoot-LeaningRoom/spring-boot-with-basic-auth 2/src/main/java/lk/ijse/gdse66/springbootwithbasicauth/SpringBootWithBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.springbootwithbasicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithBasicAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithBasicAuthApplication.class, args);
+	}
+
+}
